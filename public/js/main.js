ff4af148280e029bb2b12353e47267ffb31bb0dc
Brian Engel <brianengel@gmail.com>
2012-03-17 21:32:56 -0400

    Backbone work Created template for view that was missing template. Views now generate the html rather than it being provided from the server. Cleaned up functionality for the statbox (property display defaults and other values)

--- public/js/main.js
+++ public/js/main.js
@@ -1,71 +1,80 @@
 $(function() {
 
-	// Shamelessly stolen from the SendHub blog entry on backbone.js
-	Backbone.View.prototype.renderTemplate = function() {
-		var getTemplate = function(id) {
-			return Handlebars.compile($("#"+id).html());
-		}
+    // Shamelessly stolen from the SendHub blog entry on backbone.js
+    Backbone.View.prototype.renderTemplate = function() {
+        var getTemplate = function(id) {
+            return Handlebars.compile($("#"+id).html());
+        }
 
-		var compiledTemplates = {};
+        var compiledTemplates = {};
 
-		var fn = function(id, context) {
-			if(!_(compiledTemplates).has(id)) {
-				compiledTemplates[id] = getTemplate(id);
-			}
-			return compiledTemplates[id](context);
-		}
+        var fn = function(id, context) {
+            if(!_(compiledTemplates).has(id)) {
+                compiledTemplates[id] = getTemplate(id);
+            }
+            return compiledTemplates[id](context);
+        }
 
-		return fn;
-	}();
-
-
-	// Define our models
-	var TextStats = Backbone.Model.extend({
-		defaults: {
-			"text" : "",
-		},
-		initialize: function() {
-			this.calculateCharacters();
-		},
-		calculateCharacters: function() {
-			this.set("characterCount", this.get("text").length);
-		}
-	});
-
-	// Define our views
-	var StatBlock = Backbone.View.extend({
-		render: function() {
-			stats = new TextStats()
-			$(this.el).html(this.renderTemplate("template-stat", 
-			{
-				"characterCount" : this.model.get("characterCount")
-			}));
-			return this;
-		}
-	});
-
-	var Editor = Backbone.View.extend({
-		events: {
-			"keyup .editor": "updateStats"
-		},
-		render: function() {
-			
-			return this;
-		},
-		updateStats: function(e) {
-			var textField = $(this.el).find(".editor")[0];
-			var text = textField.value;
-			var statsModel = new TextStats({"text":text});
-
-			var statBlock = $(this.el).find(".stats")[0];
-			var statView = new StatBlock({el: statBlock, model: statsModel});
-			statView.render();
-		}
-	});
+        return fn;
+    }();
 
 
+    // Define our models
+    var TextStats = Backbone.Model.extend({
+        defaults: {
+            "text" : "",
+        },
+        initialize: function() {
+            this.calculateCharacters();
+            this.calculateWords();
+        },
+        calculateCharacters: function() {
+            this.set("characterCount", this.get("text").length);
+        },
+        calculateWords: function() {
+            var words = 0;
+            var text = this.get("text").trim();
 
-	var mainText = new TextArea();
-	var mainStatsView = new Editor({el: $('#main-editor')});
-	mainStatsView.render();
+            if (text.length > 0) {
+                words = text.split(' ').length; 
+            };
+
+            this.set("wordCount", words);
+        }
+    });
+
+    // Define our views
+    var StatBlock = Backbone.View.extend({
+        render: function() {
+            $(this.el).html(this.renderTemplate("template-stat", 
+            {
+                "characterCount": this.model.get("characterCount"),
+                "wordCount": this.model.get("wordCount")
+            }));
+            return this;
+        }
+    });
+
+    var Editor = Backbone.View.extend({
+        events: {
+            "keyup .editor": "updateStats"
+        },
+        render: function() {
+            $(this.el).html(this.renderTemplate("template-editor"), {} );
+            this.updateStats();
+            return this;
+        },
+        updateStats: function(e) {
+            var textField = $(this.el).find(".editor")[0];
+            var text = textField.value;
+            var statsModel = new TextStats({"text":text});
+
+            var statBlock = $(this.el).find(".stats")[0];
+            var statView = new StatBlock({el: statBlock, model: statsModel});
+            statView.render();
+        }
+    });
+
+    var mainStatsView = new Editor({el: '#main-editor'});
+    mainStatsView.render();
 });
